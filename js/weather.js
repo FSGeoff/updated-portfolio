b783a21301aee4b8b80dc708f42f893207bddbe7
Geoff Nix <FullStackGeoff@gmail.com>
2020-12-22 15:00:36 -0500

    work history page

--- js/weather.js
+++ js/weather.js
@@ -10,10 +10,10 @@ $(document).ready(function () {
 			for (i = 0; i < weatherArr.length; i++) {
 				console.log(weatherArr[i]);
 			}
-            const temp = Math.round(response.main.temp) + "&#176;";
-            const description = weatherArr[0].description;
-            console.log(response);
-            console.log(description);
+			const temp = Math.round(response.main.temp) + "&#176;";
+			const description = weatherArr[0].description;
+			console.log(response);
+			console.log(description);
 
 			const icon = $("<img>");
 			icon.attr("height", "16px", "width", "16px");
@@ -22,19 +22,31 @@ $(document).ready(function () {
 
 			switch (forecast) {
 				case "CLEAR":
-                    icon.attr("src", "../icons/sun.png");
-                    break;
+					icon.attr("src", "../icons/sun.png");
+					break;
 				case "CLOUDS":
-                    icon.attr("src", "../icons/cloudy.png");
-                    break;
-                    case "RAIN":
-                    icon.attr("src", "../icons/rain.png");
-                    break;
+					icon.attr("src", "../icons/cloudy.png");
+					break;
+				case "RAIN":
+					icon.attr("src", "../icons/rain.png");
+					break;
+				case "SNOW":
+					icon.attr("src", "../icons/snow.png");
+					break;
+				case "WIND":
+					icon.attr("src", "../icons/windy.png");
+					break;
+				case "WINDY":
+					icon.attr("src", "../icons/windy.png");
+					break;
+				case "THUNDER":
+					icon.attr("src", "../icons/thunder.png");
+					break;
 				default:
 					icon.attr("src", "../icons/sun.png");
 			}
-            $("#temp").append(temp, icon);
-            $("#description").append(description)
+			$("#temp").append(temp, icon);
+			$("#description").append(description);
 		});
 	}
 	keepTemp();
